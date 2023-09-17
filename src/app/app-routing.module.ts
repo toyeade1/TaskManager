bdd5149112860e64f182c7fa3c1bdbdd91ad4056
Akintoye Adesomoju <aadesomoju001@gmail.com>
2023-09-17 02:38:23 -0400

    feat(TOYE-001): Initializing project on Github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'contact', component:ContactMeComponent},
+  {path: "", redirectTo: "dashboard", pathMatch: 'full'} // this is added to make sure that if the link is put with no route then it would be redirected to the dashboard.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
